04f39162174be9c285ecd196b1d4892399178b39
youssefahmed5491 <48548301+youssefahmed5491@users.noreply.github.com>
2019-03-20 23:52:21 +0200

    Update main.java

--- ATM/src/main.java
+++ ATM/src/main.java
@@ -2,7 +2,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class main extends Application {
-	
+	public static void main (String[] args) {
+		launch(args);
+	}
 
 	@Override
 	public void start(Stage primaryStage ) throws Exception {
